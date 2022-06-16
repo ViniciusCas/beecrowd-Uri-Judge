a16d62f1e1302b01c705c1a07500a601d5a5d2c4
ViniciusCas <viniccasi@gmail.com>
2022-06-16 16:02:04 -0300

    Corigida solução 1049.c

--- beginner/1049.c
+++ beginner/1049.c
@@ -1,34 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
     char a[11], b[30], c[30];
     scanf("%s %s %s", a, b, c);
-    char d[13] = "vertebrado";
-    for(int i = 0; i <= sizeof("vertebrado"); i++){
-        printf("%c\n","vertebrado"[i]);
-    }
 
-    for(int i = 0; i <= sizeof(a); i++){
-        printf("%c\n",a[i]);
-    }
-
-    printf("%d\n", a == "vertebrado");
-    if(a == "vertebrado") {
-        if (b == "ave") {
-            if(c == "carnivoro") printf("aguia\n");
+    if(!strcmp(a, "vertebrado")) {
+        if (!strcmp(b, "ave")) {
+            if(!strcmp(c, "carnivoro")) printf("aguia\n");
             else printf("pomba\n");
         } else {
-            if(c == "onivoro") printf("homem\n");
+            if(!strcmp(c, "onivoro")) printf("homem\n");
             else printf("vaca\n");
         }         
 
-    } else if(a == ""){
-        if(b == "inseto") {
-            if(c == "hematofago") printf("pulga\n");
+    } else {
+        if(!strcmp(b, "inseto")) {
+            if(!strcmp(c, "hematofago")) printf("pulga\n");
             else printf("lagarta\n");
         } else {
-            if(c == "hematofago") printf("sanguessuga\n"); 
+            if(!strcmp(c, "hematofago")) printf("sanguessuga\n"); 
             else printf("minhoca\n");
         }       
     }
